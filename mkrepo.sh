0e3b7c5864710b8b3ff0094b291f2a384dc210c4
g5ostXa <g5ostX2@proton.me>
2024-11-08 00:25:37 -0500

    Update mkrepo.sh

--- mkrepo.sh
+++ mkrepo.sh
@@ -3,22 +3,42 @@
 # --------------------------------------------------
 # mkrepo.sh | By g5ostXa | 2024
 # --------------------------------------------------
+# Run this script in the same directory where you want your repo
+# Arguments: --repo=repo --repo-url=repourl --gitusername=username
+# Usage example:
+#   ./mkrepo.sh --repo=somerepo --repo-url=https://github.com/gitusername/somerepo.git --gitusername=gitusername
 
-# Run this script in the same directory you want your repo
-# with the REPO, REPO_URL and GIT_USERNAME as arguments.
-
-# Do not run as root.
-
-# (e.g):
-# cd ~/path/to/your-repos-dir/ && source ~/path/to/mkrepo.sh "somerepo" "https://github.com/gitusername/somerepo.git" "gitusername"
-
-REPO="$1"
-REPO_URL="$2"
-GIT_USERNAME="$3"
 GIT_CLIENT="github"
+
+for ARG in "$@"; do
+	case $ARG in
+	--repo=*)
+		REPO="${ARG#*=}"
+		shift
+		;;
+	--repo-url=*)
+		REPO_URL="${ARG#*=}"
+		shift
+		;;
+	--gitusername=*)
+		GIT_USERNAME="${ARG#*=}"
+		shift
+		;;
+	*)
+		echo "Unknown argument: $ARG"
+		echo "Usage: $0 --repo=repo_name --repo-url=repo_url --gitusername=git_username "
+		exit 1
+		;;
+	esac
+done
 
 if ! command -v git >/dev/null 2>&1; then
 	echo "git is not installed..."
+	exit 1
+fi
+
+if [ -z "$REPO" ] || [ -z "$REPO_URL" ] || [ -z "$GIT_USERNAME" ]; then
+	echo "Usage: $0 --repo=repo_name --repo-url=repo_url --gitusername=git_username "
 	exit 1
 fi
 
