31296cb34d82076215c35c6ddd9565df3853139f
g5ostXa <g5ostX2@proton.me>
2024-11-08 01:01:58 -0500

    Update mkrepo.sh

--- mkrepo.sh
+++ mkrepo.sh
@@ -4,29 +4,31 @@
 # mkrepo.sh | By g5ostXa | 2024
 # --------------------------------------------------
 # Run this script in the same directory where you want your repo
-# Arguments: --repo=repo --repo-url=repourl --gitusername=username
+# Arguments: --repo-name=NEWREPO --repo-url=REPO_URL --git-user=USERNAME --git-client=GIT_CLIENT
 # Usage example:
-#   ./mkrepo.sh --repo=somerepo --repo-url=https://github.com/gitusername/somerepo.git --gitusername=gitusername
-
-GIT_CLIENT="github"
+#   ./mkrepo.sh --repo-name=NEWREPO --repo-url=https://github.com/git-user/newrepo.git --git-user=USERNAME --git-client=github
 
 for ARG in "$@"; do
 	case $ARG in
-	--repo=*)
-		REPO="${ARG#*=}"
+	--repo-name=*)
+		REPO_NAME="${ARG#*=}"
 		shift
 		;;
 	--repo-url=*)
 		REPO_URL="${ARG#*=}"
 		shift
 		;;
-	--gitusername=*)
-		GIT_USERNAME="${ARG#*=}"
+	--git-user=*)
+		GIT_USER="${ARG#*=}"
+		shift
+		;;
+	--git-client=*)
+		GIT_CLIENT="${ARG#*=}"
 		shift
 		;;
 	*)
 		echo "Unknown argument: $ARG"
-		echo "Usage: $0 --repo=repo_name --repo-url=repo_url --gitusername=git_username "
+		echo "Usage: $0 --repo-name=NEWREPO --repo-url=REPO_URL --git-user=USERNAME --git-client=GIT_CLIENT"
 		exit 1
 		;;
 	esac
@@ -37,15 +39,15 @@ if ! command -v git >/dev/null 2>&1; then
 	exit 1
 fi
 
-if [ -z "$REPO" ] || [ -z "$REPO_URL" ] || [ -z "$GIT_USERNAME" ]; then
-	echo "Usage: $0 --repo=repo_name --repo-url=repo_url --gitusername=git_username "
+if [ -z "$REPO_NAME" ] || [ -z "$REPO_URL" ] || [ -z "$GIT_USER" ] || [ -z "$GIT_CLIENT" ]; then
+	echo "Usage: $0 --repo-name=NEWREPO --repo-url=REPO_URL --git-user=USERNAME --git-client=GIT_CLIENT"
 	exit 1
 fi
 
 set -e
 
-mkdir "$REPO"
-cd "$REPO"
+mkdir "$REPO_NAME"
+cd "$REPO_NAME"
 
 git init
 git branch -M main
@@ -56,4 +58,4 @@ echo "$REPO" >>README.md
 git add .
 git commit -S -m "initial"
 git remote add origin "$REPO_URL"
-git push --set-upstream git@"$GIT_CLIENT".com:"$GIT_USERNAME"/"$REPO".git
+git push --set-upstream git@"$GIT_CLIENT".com:"$GIT_USER"/"$REPO_NAME".git
